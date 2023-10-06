4a3adc5d2c6d63e475b35c549c3d79be583e17a0
TatianaMamaliga <125645241+TatianaMamaliga@users.noreply.github.com>
2023-10-06 19:04:09 +0300

    Initial commit

---
+++ src/main/java/com/charity/registervolunteer/RegisterVolunteerApplication.java
@@ -0,0 +1,13 @@
+package com.charity.registervolunteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterVolunteerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterVolunteerApplication.class, args);
+	}
+
+}
